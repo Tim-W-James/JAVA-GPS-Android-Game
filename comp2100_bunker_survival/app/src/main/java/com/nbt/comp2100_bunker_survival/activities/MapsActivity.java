e04a3e6bd54ccdeb47c5314d290493e321f83ba1
Tim James <u6947396@anu.edu.au>
2020-04-22 16:13:41 +1000

    organized package structure, added basic classes, updated Android Studio to 3.6.3

--- comp2100_bunker_survival/app/src/main/java/com/nbt/comp2100_bunker_survival/activities/MapsActivity.java
+++ comp2100_bunker_survival/app/src/main/java/com/nbt/comp2100_bunker_survival/activities/MapsActivity.java
@@ -1,4 +1,4 @@
-package com.nbt.comp2100_bunker_survival;
+package com.nbt.comp2100_bunker_survival.activities;
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -10,6 +10,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.nbt.comp2100_bunker_survival.R;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
---
+++ comp2100_bunker_survival/app/src/main/java/com/nbt/comp2100_bunker_survival/model/Event.java
@@ -0,0 +1,4 @@
+package com.nbt.comp2100_bunker_survival.model;
+
+public abstract class Event {
+}
