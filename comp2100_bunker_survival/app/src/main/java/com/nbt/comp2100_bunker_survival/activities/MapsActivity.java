d4f0927c3a753c23d0ca3e32c147148f147a2a39
Byron Dobner <u6426074@anu.edu.au>
2020-05-06 19:22:48 +1000

    Initial location tracking

--- comp2100_bunker_survival/app/src/main/java/com/nbt/comp2100_bunker_survival/activities/MapsActivity.java
+++ comp2100_bunker_survival/app/src/main/java/com/nbt/comp2100_bunker_survival/activities/MapsActivity.java
@@ -2,19 +2,37 @@ package com.nbt.comp2100_bunker_survival.activities;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.nbt.comp2100_bunker_survival.R;
+
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private Marker playerMarker;
+    private Circle circle;
+    private float x = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,25 +42,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Disabling the ability to pan the map around
+        mMap.getUiSettings().setScrollGesturesEnabled(false);
+
+        //TODO - You can add a tag to the circle, which means that you can check when you pick something up if it is in the circle
+        circle = mMap.addCircle(new CircleOptions()
+                .center(new LatLng(0, 0))
+                .radius(25)
+                .strokeColor(Color.argb(100, 0, 0, 255))
+                .fillColor(Color.argb(40, 0, 0, 255)));
+
+        //Creating the marker which shows the players current location
+        MarkerOptions playerMark = new MarkerOptions().position(new LatLng(0, 0));
+        playerMarker = mMap.addMarker(playerMark);
+
+        //TODO - set the player Icon
+        //playerMarker.setIcon();
+
+
+        //Zooming the camera to a more appropriate level
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(18));
+        locationHandler();
+    }
+
+    //Initializes a handler which runs every second, and calls updateLocation();
+    public void locationHandler(){
+        final Handler handler = new Handler();
+        final Runnable locationUpdater = new Runnable() {
+            public void run()
+            {
+                updateLocation();
+                handler.postDelayed(this, 1000);
+            }
+        };
+        handler.postDelayed(locationUpdater, 1000);
+    }
+
+    public void updateLocation(){
+        //Checking if the app has permission to use location information
+        if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || checkSelfPermission(Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}, 10);
+        }
+
+        Location currentLocation = locationManager.getLastKnownLocation(locationManager
+                .getBestProvider(new Criteria(), false));
+        LatLng currentLatLng = new LatLng(currentLocation.getLatitude() + x,currentLocation.getLongitude());
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(currentLatLng), 500, null);
+
+        //TODO - move this in small increments so that it smoothly transitions over 500ms, the same as the camera above
+        //Will require pulling apart currentLatLng, and playerMarket.getPosition(), and calculating how far it should move over a number of steps
+        playerMarker.setPosition(currentLatLng);
+        circle.setCenter(currentLatLng);
     }
 }
