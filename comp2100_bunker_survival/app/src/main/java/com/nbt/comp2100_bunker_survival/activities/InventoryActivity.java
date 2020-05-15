e73d249718b4357e9415519f9fe85e91caa01d40
u6430146@anu.edu.au <ShadowHawk07@users.noreply.github.com>
2020-05-15 16:56:26 +1000

    Inventory boilerplate code

---
+++ comp2100_bunker_survival/app/src/main/java/com/nbt/comp2100_bunker_survival/activities/InventoryActivity.java
@@ -0,0 +1,16 @@
+package com.nbt.comp2100_bunker_survival.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.nbt.comp2100_bunker_survival.R;
+
+
+import android.os.Bundle;
+
+public class InventoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inventory);
+    }
+}
