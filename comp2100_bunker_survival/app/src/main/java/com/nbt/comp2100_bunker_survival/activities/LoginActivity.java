c29933e6b20183c889f39a1d9b6afa062eec762e
u6430146@anu.edu.au <ShadowHawk07@users.noreply.github.com>
2020-05-29 13:49:43 +1000

    login activity

---
+++ comp2100_bunker_survival/app/src/main/java/com/nbt/comp2100_bunker_survival/activities/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.nbt.comp2100_bunker_survival.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
