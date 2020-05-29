ae0ca03d1bc98185c9066f6a62fff958af163e0b
u6430146@anu.edu.au <ShadowHawk07@users.noreply.github.com>
2020-05-29 13:59:35 +1000

    more login boilerplate code

--- comp2100_bunker_survival/app/src/main/java/com/nbt/comp2100_bunker_survival/activities/LoginActivity.java
+++ comp2100_bunker_survival/app/src/main/java/com/nbt/comp2100_bunker_survival/activities/LoginActivity.java
@@ -3,6 +3,8 @@ package com.nbt.comp2100_bunker_survival.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.nbt.comp2100_bunker_survival.R;
+
 
 public class LoginActivity extends AppCompatActivity {
 
