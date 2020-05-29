6be4b8e5f57f39fe0339dec6b5c5df2bbf1dda28
Tim James <u6947396@anu.edu.au>
2020-05-29 15:16:43 +1000

    added unique treasure icons

---
+++ comp2100_bunker_survival/app/src/main/java/com/nbt/comp2100_bunker_survival/model/TreasureType.java
@@ -0,0 +1,9 @@
+package com.nbt.comp2100_bunker_survival.model;
+
+// enum for the type of resource the treasure is focused on
+public enum TreasureType {
+    FOOD,
+    SCRAP_METAL,
+    TOILET_PAPER,
+    OTHER;
+}
